243a324c59e2b0d161af10659fac380fc62f152e
joanpardogine <joanpardo@ginebro.cat>
2021-10-21 18:00:09 +0200

    Enunciat Enu01_Hipotenusa.java

---
+++ src/Enu01_Hipotenusa.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Enu01_Hipotenusa {
+    /*
+     * Fer un programa que donat la base i l’altura d’un triangle rectangle, calculi
+     * la seva hipotenusa i la mostri per pantalla.
+     */
+    public static void main(String[] args) throws Exception {
+        double base, alssada;
+        float hipotenusa;
+
+        Scanner teclat = new Scanner(System.in);
+        System.out.print("Entra la base del triangle: ");
+        base = teclat.nextInt();
+
+        System.out.print("Entra l'alçada del triangle: ");
+        alssada = teclat.nextInt();
+
+        hipotenusa = (float) Math.sqrt(Math.pow(base, 2) + Math.pow(alssada, 2));
+        // hipotenusa = (float) Math.sqrt(base * base + alssada * alssada);
+        // hipotenusa = sqrt ( base^2 + alssada^2)
+        System.out.println("La hipotenusa és " + hipotenusa);
+
+        teclat.close();
+    }
+}
