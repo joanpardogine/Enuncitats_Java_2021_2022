d60073f222f746e1ceea13890531c8f30786fd30
joanpardogine <joanpardo@ginebro.cat>
2021-10-22 17:28:51 +0200

    Teoria bucles 03 i 04

---
+++ src/Teoria_04_Arrays.java
@@ -0,0 +1,21 @@
+public class Teoria_04_Arrays {
+    public static void main(String[] args) {
+        int[] arrayExemple = new int[150]; //Declarant i dimensionant l'array
+		int novaLongitud = 200;
+		
+        //Redimensionant l'array
+		arrayExemple = new int[novaLongitud];
+		
+        //Declaro,dimensiono i Inicialitzo.
+		String[] diesSetmana = {"Dilluns","Dimarts","Dimecres","Dijous","Divendres",
+								"Dissabte","Diumenge"};
+		
+		System.out.println("L'element 3 de l'array és "+diesSetmana[2]);
+		diesSetmana[2] = "Wednesday";
+		System.out.println("L'element 3 de l'array és "+diesSetmana[2]);
+		
+		//Recorregut array.
+		for (int i=0;i<diesSetmana.length;i++) {
+			System.out.println(diesSetmana[i]);
+
+		}
