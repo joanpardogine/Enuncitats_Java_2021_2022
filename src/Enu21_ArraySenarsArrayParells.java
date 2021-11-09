c15b11e6ce98a3b7fb6c2b51764c0d9f3834d26f
joanpardogine <joanpardo@ginebro.cat>
2021-11-09 19:52:42 +0100

    Corregit Enu21_ArraySenarsArrayParells!

--- src/Enu21_ArraySenarsArrayParells.java
+++ src/Enu21_ArraySenarsArrayParells.java
@@ -1,3 +1,25 @@
-import java.util.Scanner;
-
 public class Enu21_ArraySenarsArrayParells {
+    public static void main(String args[]) throws Exception {
+        int[] arraySenars = new int[5];
+        int[] arrayParells = new int[5];
+        int nombreAleatori, poscArraySenars = 0, poscArrayParell = 0;
+        do {
+            nombreAleatori = (int) (Math.random() * 100);
+            if (nombreAleatori % 2 == 0) {
+                if (poscArrayParell < 5) {
+                    arrayParells[poscArrayParell] = nombreAleatori;
+                    poscArrayParell++;
+                }
+            } else {
+                if (poscArraySenars < 5) {
+                    arraySenars[poscArraySenars] = nombreAleatori;
+                    poscArraySenars++;
+                }
+            }
+        } while ((poscArrayParell + poscArraySenars) < 10);
+        System.out.println("par | imp");
+        for (int i = 0; i < arraySenars.length; i++) {
+            System.out.println(arrayParells[i] + " | " + arraySenars[i]);
+        }
+    }
+}
