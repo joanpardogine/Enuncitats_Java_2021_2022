38c61151749615ac2cf4495b6c2da0e6499028bd
joanpardogine <joanpardo@ginebro.cat>
2021-11-09 18:27:01 +0100

    modified:   A_Teoria_06A_TryCatch.java

--- src/A_Teoria_06A_TryCatch.java
+++ src/A_Teoria_06A_TryCatch.java
@@ -7,6 +7,8 @@ public class A_Teoria_06A_TryCatch {
         } catch (Exception errorAGestionar) {
             System.out.println("Alguna cosa ha fallat.");
             System.out.println("L'error ha estat " + errorAGestionar.getMessage());
+        } finally {
+            System.out.println("La comanda 'try catch' ha acabat!.");
         }
     }
 }
