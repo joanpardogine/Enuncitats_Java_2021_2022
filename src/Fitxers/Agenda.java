f62bbdb206e4698188435929e8bdd122523df1dc
joanpardogine <joanpardo@ginebro.cat>
2022-02-08 19:55:46 +0100

    modified:   src/Fitxers/Agenda.java

--- src/Fitxers/Agenda.java
+++ src/Fitxers/Agenda.java
@@ -1,9 +1,15 @@
 package Fitxers;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Agenda {
+    // Joan;Pardo;936501441;joan@pardo.cat;Avinguda Diagonal
+    // 0    1     2         3              4
+
+    static final int QTAT_CAMPS = 5;
+    static final String SEPARADOR = ";";
 
     static final int NOM = 0;
     static final int COGNOM = 1;
@@ -13,8 +19,6 @@ public class Agenda {
 
     static final int CATALA = 0;
     static final int ESPANYOL = 1;
-
-    static final int QTAT_CAMPS = 5;
 
     static String nomFitxer = "src\\Fitxers\\agenda.csv";
     static Scanner teclat = new Scanner(System.in);
@@ -51,9 +55,16 @@ public class Agenda {
     }
 
     public static void mostraAgenda(String[][] agenda) {
+        Ordres.netejaConsola();
         for (int i = 0; i < agenda.length; i++) {
-            System.out.println((i + 1) + agenda[i][0] + " -" + agenda[i][1] + " - " + agenda[i][2]);
+            System.out.println((i + 1) + " " +
+                    agenda[i][NOM] + " - " +
+                    agenda[i][COGNOM] + " - " +
+                    agenda[i][TELEFON] + " - " +
+                    agenda[i][CORREU] + " - " +
+                    agenda[i][ADRESA]);
         }
+        Ordres.pausa("\nPitja un tecla per continuar!");
     }
 
     public static void omplirMatriu(String[][] agenda) {
@@ -67,7 +78,7 @@ public class Agenda {
             String array[];
             for (int i = 0; i < contaLinies(); i++) {
                 linia = punter.nextLine(); // Salvem la linia dins d'un String
-                array = linia.split(",");
+                array = linia.split(SEPARADOR);
                 agenda[i][NOM] = array[NOM];
                 agenda[i][COGNOM] = array[COGNOM];
                 agenda[i][TELEFON] = array[TELEFON];
@@ -88,15 +99,17 @@ public class Agenda {
         }
     }
 
-    static void mostraMenu(String[][] llistaAMostrar, int idioma) {
+    static void mostraMenu(String[] llistaAMostrar) { //, int idioma) {
         String opcions = "";
         for (int i = 0; i < llistaAMostrar.length; i++) {
-            System.out.println((i + 1) + " - " + llistaAMostrar[i][idioma]);
+            System.out.println((i + 1) + " - " + llistaAMostrar[i]);
+            // System.out.println((i + 1) + " - " + llistaAMostrar[i][idioma]);
             opcions += (i + 1);
             if (i < llistaAMostrar.length - 1) {
                 opcions += ",";
             }
         }
+        teclat = new Scanner(System.in);
         System.out.print("Entra una de les opcions (" + opcions + "): ");
     }
 
@@ -112,10 +125,57 @@ public class Agenda {
                         + "\n        Només pots entrar nombres!"
                         + "\n        Torna a intentar-ho: ");
                 esEnter = false;
-                teclat.nextLine();
+            } finally {
+                teclat = new Scanner(System.in);
             }
         } while (esEnter == false);
         return nombreLlegit;
+    }
+
+    public static String[][] afegirContacte(String[][] agenda) {
+        String nomLlegit;
+        String[][] novaAgenda = new String[agenda.length + 1][QTAT_CAMPS];
+        for (int i = 0; i < agenda.length; i++) {
+            for (int j = 0; j < agenda[i].length; j++) {
+                novaAgenda[i][j] = agenda[i][j];
+            }
+        }
+
+        mostrarMissatge("Entra el nom del nou contacte: ");
+        novaAgenda[agenda.length][NOM] = teclat.next();
+        mostrarMissatge("Entra el cognom del nou contacte: ");
+        novaAgenda[agenda.length][COGNOM] = teclat.next();
+        mostrarMissatge("Entra el telefon del nou contacte: ");
+        novaAgenda[agenda.length][TELEFON] = teclat.next();
+        mostrarMissatge("Entra el correu del nou contacte: ");
+        novaAgenda[agenda.length][CORREU] = teclat.next();
+        mostrarMissatge("Entra l'adreça del nou contacte: ");
+        novaAgenda[agenda.length][ADRESA] = teclat.next();
+
+        return novaAgenda;
+    }
+
+    public static void guardarAgenda(String[][] agenda) {
+        FileWriter fitxer = null;
+        String contacteAGuardar = "";
+        try {
+            fitxer = new FileWriter(nomFitxer);
+
+            // Escrivim línia a línia en el fitxer
+            for (int i = 0; i < agenda.length; i++) { // String linia : paraules) {
+                contacteAGuardar = "";
+                contacteAGuardar += agenda[i][NOM] + SEPARADOR;
+                contacteAGuardar += agenda[i][COGNOM] + SEPARADOR;
+                contacteAGuardar += agenda[i][TELEFON] + SEPARADOR;
+                contacteAGuardar += agenda[i][CORREU] + SEPARADOR;
+                contacteAGuardar += agenda[i][ADRESA] + SEPARADOR;
+
+                fitxer.write(contacteAGuardar + "\n");
+            }
+            fitxer.close();
+        } catch (Exception exepcio) {
+            System.out.println("Missatge de l'excepció: " + exepcio.getMessage());
+        }
     }
 
     public static void main(String[] args) {
@@ -124,30 +184,39 @@ public class Agenda {
         omplirMatriu(agenda);
 
         int opcio = 0;
-        String[][] operacions = {
-                { "Afegir contacte.", "Añadir contacto." },
-                { "Esborrar contacte.", "Borrar contacto." },
-                { "Modificar contacte.", "Modificar contacto." },
-                { "Mostrar String[]s.", "Mostrar contactos." },
-                { "Sortir i guardar dades", "Salir y guardar datos." },
-                { "Sortir sense guardar.", "Salir sin guardar." } };
+        String[] operacions = {
+                "Afegir contacte.",
+                "Esborrar contacte.",
+                "Modificar contacte.",
+                "Mostrar contactes.",
+                "Sortir i guardar dades",
+                "Sortir sense guardar." };
+        // String[][] operacions = {
+        //         { "Afegir contacte.", "Añadir contacto." },
+        //         { "Esborrar contacte.", "Borrar contacto." },
+        //         { "Modificar contacte.", "Modificar contacto." },
+        //         { "Mostrar contactes.", "Mostrar contactos." },
+        //         { "Sortir i guardar dades", "Salir y guardar datos." },
+        //         { "Sortir sense guardar.", "Salir sin guardar." } };
 
-        int idioma;
+        // int idioma;
         boolean volsSortir = false;
-        mostrarMissatge("\nEn quin idioma vols el menú? (" + CATALA + " - Cat, " + ESPANYOL + " - Esp):");
+        // mostrarMissatge("\nEn quin idioma vols el menú? (" + CATALA + " - Cat, " + ESPANYOL + " - Esp):");
         System.out.println();
 
-        do {
-            idioma = llegirEnterTeclat();
-        } while (idioma != CATALA && idioma != ESPANYOL);
+        // do {
+        //     idioma = llegirEnterTeclat();
+        // } while (idioma != CATALA && idioma != ESPANYOL);
 
         do {
-
-            mostraMenu(operacions, idioma);
-            opcio = llegirEnterTeclat();
+            Ordres.netejaConsola();
+            mostraMenu(operacions); // , idioma);
+            // opcio = llegirEnterTeclat();
+            opcio = teclat.nextInt();
 
             switch (opcio) {
                 case 1: // Afegir
+                    agenda = afegirContacte(agenda);
                     System.out.println(operacions[opcio - 1]);
                     break;
                 case 2: // Esborrar
@@ -160,10 +229,10 @@ public class Agenda {
                     mostraAgenda(agenda);
                     break;
                 case 5: // Guardar + sortir
-                    System.out.println(operacions[opcio - 1]);
+                    guardarAgenda(agenda);
                     volsSortir = true;
                     break;
-                case 6: // ortir sense guardar
+                case 6: // Sortir sense guardar
                     System.out.println(operacions[opcio - 1]);
                     volsSortir = true;
                     break;
