f1abd21f2a1e9c3ba7924935c67dfb190aa2b10b
joanpardogine <joanpardo@ginebro.cat>
2022-04-01 15:07:40 +0200

    modified:   src/Objectes/Biblioteca!

--- src/Objectes/Biblioteca/Biblioteca.java
+++ src/Objectes/Biblioteca/Biblioteca.java
@@ -4,32 +4,20 @@ import java.util.Arrays;
 
 public class Biblioteca {
     private String nom;
-    // private int qtatMaxima;
     private int qtatLlibres;
     private Llibre[] prestatge;
 
-    // public Biblioteca(String nom, int qtatMaxima) {
     public Biblioteca(String nom) {
         this.nom = nom;
-        // this.qtatMaxima = qtatMaxima;
         this.qtatLlibres = 0;
         this.prestatge = new Llibre[qtatLlibres];
     }
 
-    // public Biblioteca(String nom, int qtatMaxima, Llibre[] prestatge) {
     public Biblioteca(String nom, Llibre[] prestatge) {
         this.nom = nom;
-        // this.qtatMaxima = qtatMaxima;
         this.qtatLlibres = prestatge.length - 1;
         this.prestatge = prestatge;
     }
-
-    // public Biblioteca(String nom, Llibre[] prestatge) {
-    //     this.nom = nom;
-    //     this.qtatMaxima = 100;
-    //     this.prestatge = prestatge;
-    //     this.qtatLlibres = 0;
-    // }
 
     public String getNom() {
         return this.nom;
@@ -39,14 +27,6 @@ public class Biblioteca {
         this.nom = nom;
     }
 
-    // public int getQtatMaxima() {
-    //     return this.qtatMaxima;
-    // }
-
-    // public void setQtatMaxima(int qtatMaxima) {
-    //     this.qtatMaxima = qtatMaxima;
-    // }
-
     public Llibre[] getPrestatge() {
         return this.prestatge;
     }
@@ -55,10 +35,10 @@ public class Biblioteca {
         this.prestatge = prestatge;
     }
 
-    public void llistatDeLlibresPerTitol(){
+    public void llistatDeLlibresPerTitol() {
         for (int i = 0; i < this.prestatge.length; i++) {
-            System.out.println("Llibre número " + (i+1) + ": " +
-            this.getPrestatge()[i].getTitol());
+            System.out.println("Llibre número " + (i + 1) + ": " +
+                    this.getPrestatge()[i].getTitol());
         }
     }
 
@@ -70,10 +50,9 @@ public class Biblioteca {
         }
         return "Nom biblioteca = " + this.nom + "\n"
                 + "prestatge=" + Arrays.toString(this.prestatge);
-        // + "qtatMaxima=" + this.qtatMaxima;
     }
 
-    private int buscaLlibre(String titolLlibreABuscar) {
+    private int buscaLlibrePerTitol(String titolLlibreABuscar) {
         if (this.prestatge.length != 0) {
             for (int i = 0; i < prestatge.length; i++) {
                 if (prestatge[i].getTitol().equals(titolLlibreABuscar)) {
@@ -84,51 +63,45 @@ public class Biblioteca {
         return -1;
     }
 
-    public void afegirLlibre(Llibre prestatgeRebut) {
-        if (buscaLlibre(prestatgeRebut.getTitol()) == -1) {
-            Llibre[] prestatgeAuxiliar = new Llibre[this.qtatLlibres + 1];
+    public Llibre tornaLlibre(int posicioLlibreABuscar) {
+        if (posicioLlibreABuscar < this.qtatLlibres) {
+            return prestatge[posicioLlibreABuscar];
+        }
+        return null;
+    }
+    
+    public void afegirLlibre(Llibre llibreRebut) {
+        int novaMida = this.qtatLlibres + 1;
+        if (buscaLlibrePerTitol(llibreRebut.getTitol()) == -1) {
+            Llibre[] prestatgeGran = new Llibre[novaMida];
             for (int i = 0; i < this.qtatLlibres; i++) {
-                prestatgeAuxiliar[i] = this.prestatge[i];
+                prestatgeGran[i] = this.prestatge[i];
             }
-            prestatgeAuxiliar[prestatgeAuxiliar.length - 1] = prestatgeRebut;
-            this.prestatge = prestatgeAuxiliar;
-            this.qtatLlibres++;
+            prestatgeGran[prestatgeGran.length - 1] = llibreRebut;
+            this.prestatge = prestatgeGran;
+            this.qtatLlibres = novaMida;
             System.out.println("El llibre amb titol " +
-            prestatgeRebut.getTitol() +
-            " afegit correctament!");
+                    llibreRebut.getTitol() +
+                    " afegit correctament!");
         } else { // si que existeix xq és diferent de -1
             System.out.println("El llibre amb titol " +
-                    prestatgeRebut.getTitol() +
+                    llibreRebut.getTitol() +
                     " ja existeix!");
         }
     }
 
     public void esborrarLlibre(Llibre llibreRebut) {
-        // Pendent de desenvolupar
-        this.qtatLlibres = 1;
+        int novaMida = this.qtatLlibres - 1;
+        Llibre[] prestatgePetit = new Llibre[novaMida];
+        int comptadorExtra = 0;
+        for (int i = 0; i < novaMida; i++) {
+            if (this.prestatge[i + comptadorExtra].equals(llibreRebut)) {
+                comptadorExtra--;
+            } else {
+                prestatgePetit[i + comptadorExtra] = this.prestatge[i];
+            }
+        }
+        this.prestatge = prestatgePetit;
+        this.qtatLlibres = novaMida ;
     }
-
 }
-
-// public Llibre[] esborrarLlibre(Llibre[] agenda) {
-//     String[][] novaAgenda = new String[agenda.length - 1][QTAT_CAMPS];
-//     int contacteAEliminar;
-//     int posNovaAgenda = 0;
-//     mostraAgenda(agenda);
-//     mostrarMissatge("Quin contacte vols eliminar: ");
-
-//     contacteAEliminar = teclat.nextInt();
-
-//     for (int i = 0; i < agenda.length; i++) {
-//         for (int j = 0; j < agenda[i].length; j++) {
-//             if (i != contacteAEliminar - 1) {
-//                 novaAgenda[posNovaAgenda][j] = agenda[i][j];
-//             }
-//         }
-//         if (i != contacteAEliminar - 1) {
-//             posNovaAgenda++;
-//         }
-//     }
-
-//     return novaAgenda;
-// }
--- src/Objectes/Biblioteca/TestBiblioteca.java
+++ src/Objectes/Biblioteca/TestBiblioteca.java
@@ -1,32 +1,59 @@
 package Objectes.Biblioteca;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TestBiblioteca {
-    
+    static Scanner teclat = new Scanner(System.in);
+
     public static void main(String[] args) {
         boolean esEnter = false;
-        
         Biblioteca biblio1 = new Biblioteca("Biblioteca de prova");
-        
-       System.out.print("Mostra dades 1\n" + biblio1.toString());
-       
-       Llibre dqdlm = new Llibre("Don Quijote de la Mancha",
-       "Miguel de Cervantes Saavedra", 2, 0);
-       
-       Llibre dqdlm2 = new Llibre("La isla del tesoro",
-       "Robert Louis Stevenson", 4, 0);
-       
-       //    biblio1.setPrestatge(biblio1.afegeixLlibreImanol(dqdlm));
-       //    biblio1.afegirLlibre(dqdlm2);
-       System.out.println( "\n Anem a afegir el primer dels llibre!");
-       biblio1.afegirLlibre(dqdlm);
-       System.out.println( "\n Anem a afegir el primer dels llibre per 2a vegada!");
-       biblio1.afegirLlibre(dqdlm);
-       System.out.println( "\n Anem a afegir el segon dels llibre!");
-       biblio1.afegirLlibre(dqdlm2);
 
-       System.out.println("Mostra dades 2\n" + biblio1.toString());
+        System.out.print("Mostra dades 1\n" + biblio1.toString());
 
-       biblio1.llistatDeLlibresPerTitol();
+        Llibre dqdlm = new Llibre("Don Quijote de la Mancha",
+                "Miguel de Cervantes Saavedra", 2, 0);
+
+        Llibre dqdlm2 = new Llibre("La isla del tesoro",
+                "Robert Louis Stevenson", 4, 0);
+
+        //    biblio1.setPrestatge(biblio1.afegeixLlibreImanol(dqdlm));
+        //    biblio1.afegirLlibre(dqdlm2);
+        System.out.println("\n Anem a afegir el primer dels llibre!");
+        biblio1.afegirLlibre(dqdlm);
+        System.out.println("\n Anem a afegir el primer dels llibre per 2a vegada!");
+        biblio1.afegirLlibre(dqdlm);
+        System.out.println("\n Anem a afegir el segon dels llibre!");
+        biblio1.afegirLlibre(dqdlm2);
+
+        System.out.println("Mostra dades 2\n" + biblio1.toString());
+
+        int posicioLlibreAEsborrar = 0;
+        boolean nombreCorrecte = false;
+        int qtatLlibres;
+        do {
+            biblio1.llistatDeLlibresPerTitol();
+            nombreCorrecte = false;
+            qtatLlibres = biblio1.getPrestatge().length;
+            System.out.print("Entra el número de llibre a esborrar: ");
+            try {
+                posicioLlibreAEsborrar = teclat.nextInt();
+                nombreCorrecte = true;
+                if (!(posicioLlibreAEsborrar > 0 &&
+                        posicioLlibreAEsborrar < qtatLlibres + 1)) {
+                    System.out.println("Error no és un nombre correcte!\nTorna a intentar-ho!");
+                    nombreCorrecte = false;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error no és un nombre!\nTorna a intentar-ho!");
+            } finally {
+                teclat.nextLine();
+            }
+        } while (nombreCorrecte == false);
+        Llibre llibreAEsborrar = biblio1.tornaLlibre(posicioLlibreAEsborrar-1);
+        biblio1.esborrarLlibre(llibreAEsborrar);
+        System.out.println("Mostra dades 3\n" + biblio1.toString());
     }
 
 }
